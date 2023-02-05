e4bcf8856777ed993c4ac202447acbe10c17cf64
EKH0100 <122951863+EKH0100@users.noreply.github.com>
2023-02-05 00:26:32 -0800

    Update 2
    Minor Bug Changes

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function submitAnswers() {
   let result = `Age Group: ${ages.value} | Current Model: ${models.value} | Current Version: ${versions.value} | Current Storage Size: ${sizes.value}`;
   quesResults.push(result);
   console.log(quesResults);
-  document.getElementById("timeToUpdate").innerHTML = `It's time to update your ${models.value}${versions.value}! `;
+  document.getElementById("timeToUpdate").innerHTML = `It's time to update your ${models.value} ${versions.value}! `;
   alert('Thank you for your cooperation, please click on "Results" to answer follow up question to win a new Iphone!');
 }
 let resResults = [];
@@ -25,8 +25,8 @@ function submitInfo() {
   const fname = document.getElementById("fname").value;
   const lname = document.getElementById("lname").value;
   const email = document.getElementById("email").value;
-  let rResult = `${fname} ${lname} from age group ${ages.value} owns a ${models.value} ${versions.value} with ${sizes.value}. Here's the e-mail address for ${fname} ${lname}: ${email}`
-  document.getElementById("surveyResults").innerHTML = rResult
+  let rResult = `${fname} ${lname} from age group ${ages.value} owns an ${models.value} ${versions.value} with size of ${sizes.value}. Here's the e-mail address for ${fname} ${lname}: ${email}`
+  document.getElementById("surveyResults").innerHTML = rResult;
   resResults.push(rResult);
     alert(`Thank you for your cooperation! ${fname} ${lname}, a comfirmation email has been sent to ${email}, please check your mail box for further notice!`)
 }
