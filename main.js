afdf8e275703802c1607f79c7a5234b3789c5e6b
EKH0100 <122951863+EKH0100@users.noreply.github.com>
2023-02-05 00:16:32 -0800

    Updated Javascript
    the two function for creepy info gathering has been placed into this page.

--- main.js
+++ main.js
@@ -4,3 +4,29 @@ month = date.toLocaleString('default', {month: 'long'});
 day = date.getDate();
 document.getElementById("currentDate").innerHTML = month + " " + day + ", " + year;
 
+let quesResults = [];
+function submitAnswers() {
+  let ages = document.querySelector('input[name="age"]:checked');
+  let models = document.querySelector('input[name="model"]:checked');
+  let versions = document.querySelector('input[name="ver"]:checked');
+  let sizes = document.querySelector('input[name="size"]:checked');
+  let result = `Age Group: ${ages.value} | Current Model: ${models.value} | Current Version: ${versions.value} | Current Storage Size: ${sizes.value}`;
+  quesResults.push(result);
+  console.log(quesResults);
+  document.getElementById("timeToUpdate").innerHTML = `It's time to update your ${models.value}${versions.value}! `;
+  alert('Thank you for your cooperation, please click on "Results" to answer follow up question to win a new Iphone!');
+}
+let resResults = [];
+function submitInfo() {
+  let ages = document.querySelector('input[name="age"]:checked');
+  let models = document.querySelector('input[name="model"]:checked');
+  let versions = document.querySelector('input[name="ver"]:checked');
+  let sizes = document.querySelector('input[name="size"]:checked');
+  const fname = document.getElementById("fname").value;
+  const lname = document.getElementById("lname").value;
+  const email = document.getElementById("email").value;
+  let rResult = `${fname} ${lname} from age group ${ages.value} owns a ${models.value} ${versions.value} with ${sizes.value}. Here's the e-mail address for ${fname} ${lname}: ${email}`
+  document.getElementById("surveyResults").innerHTML = rResult
+  resResults.push(rResult);
+    alert(`Thank you for your cooperation! ${fname} ${lname}, a comfirmation email has been sent to ${email}, please check your mail box for further notice!`)
+}
