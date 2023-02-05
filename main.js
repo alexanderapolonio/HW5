b967cbb3217ead3b20174c319ff3fba08ea22eaf
EKH0100 <122951863+EKH0100@users.noreply.github.com>
2023-02-05 00:50:21 -0800

    JS Update JQuery
    I forgot to make it JQuery style.

--- main.js
+++ main.js
@@ -6,27 +6,28 @@ document.getElementById("currentDate").innerHTML = month + " " + day + ", " + ye
 
 let quesResults = [];
 function submitAnswers() {
-  let ages = document.querySelector('input[name="age"]:checked');
-  let models = document.querySelector('input[name="model"]:checked');
-  let versions = document.querySelector('input[name="ver"]:checked');
-  let sizes = document.querySelector('input[name="size"]:checked');
-  let result = `Age Group: ${ages.value} | Current Model: ${models.value} | Current Version: ${versions.value} | Current Storage Size: ${sizes.value}`;
+  let ages = $('input[name="age"]:checked');
+  let models = $('input[name="model"]:checked');
+  let versions = $('input[name="ver"]:checked');
+  let sizes = $('input[name="size"]:checked');
+  let result = `Age Group: ${ages.val()} | Current Model: ${models.val()} | Current Version: ${versions.val()} | Current Storage Size: ${sizes.val()}`;
   quesResults.push(result);
   console.log(quesResults);
-  document.getElementById("timeToUpdate").innerHTML = `It's time to update your ${models.value} ${versions.value}! `;
+  $('#timeToUpdate').html(`It's time to update your ${models.val()} ${versions.val()}! `);
   alert('Thank you for your cooperation, please click on "Results" to answer follow up question to win a new Iphone!');
 }
+
 let resResults = [];
 function submitInfo() {
-  let ages = document.querySelector('input[name="age"]:checked');
-  let models = document.querySelector('input[name="model"]:checked');
-  let versions = document.querySelector('input[name="ver"]:checked');
-  let sizes = document.querySelector('input[name="size"]:checked');
-  const fname = document.getElementById("fname").value;
-  const lname = document.getElementById("lname").value;
-  const email = document.getElementById("email").value;
-  let rResult = `${fname} ${lname} from age group ${ages.value} owns an ${models.value} ${versions.value} with size of ${sizes.value}. Here's the e-mail address for ${fname} ${lname}: ${email}`
-  document.getElementById("surveyResults").innerHTML = rResult;
+  let ages = $('input[name="age"]:checked');
+  let models = $('input[name="model"]:checked');
+  let versions = $('input[name="ver"]:checked');
+  let sizes = $('input[name="size"]:checked');
+  const fname = $("#fname").val();
+  const lname = $("#lname").val();
+  const email = $("#email").val();
+  let rResult = `${fname} ${lname} from age group ${ages.val()} owns an ${models.val()} ${versions.val()} with size of ${sizes.val()}. Here's the e-mail address for ${fname} ${lname}: ${email}`
+  $("#surveyResults").html(rResult);
   resResults.push(rResult);
-    alert(`Thank you for your cooperation! ${fname} ${lname}, a comfirmation email has been sent to ${email}, please check your mail box for further notice!`)
+  alert(`Thank you for your cooperation! ${fname} ${lname}, a confirmation email has been sent to ${email}, please check your mail box for further notice!`)
 }
